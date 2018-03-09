df73f95a23039f2ff73dfd4ea4c7e6ba8335d76f
angcbi <angcbi@126.com>
2018-03-09 09:52:36 +0000

    add shell sort

--- sort/01_bubblesort.py
+++ sort/01_bubblesort.py
@@ -76,20 +76,54 @@ def insertSort(alist):
     """
     插入排序是把元素插入到已经排好序的列表中，列表中插入位置之后的元素后以一位，需要循环n-1次
     需要两个变量，一个保存i的值，另外一个保存待插入的位置
+    最好情况，数据已经排好序， O(n^2)
     """
     for i in range(1, len(alist)):
         temp_index = i
         temp = alist[i]
-        for j in range(i):
-            # 从后往前比较
-            if alist[i-j-1] > temp:
-                alist[temp_index] = alist[i-j-1]
-                temp_index = i-j-1
+        # for j in range(i):
+        #     # 从后往前比较
+        #     if alist[i-j-1] > temp:
+        #         alist[temp_index] = alist[i-j-1]
+        #         temp_index = i-j-1
+        # 采用while循环，减少内层循环次数
+        while temp_index > 0 and alist[temp_index] > temp:
+            alist[temp_index] = alist[temp_index]
+            temp_index = temp_index - 1
 
         # 把i插入到正确位置
         alist[temp_index] = temp
 
     return alist
+
+@log
+def shellSort(alist):
+    """
+    希尔排序块是因为，一次移动了一大步逆序(如升序排列， 后面数字比前面数字大叫逆序)， 冒泡排序每次只能移动一次逆序
+    步长公式  2^k - 1 (1, 3, 7, 15)
+    """
+    length = len(alist)
+    # 迭代过程中步长变小，当步长为1时，即为一次完整的插入排序
+    # 有几个步长，就划分几个区间
+    gap = length // 2
+
+    while gap > 0:
+        # 对每个区间进行插入排序
+        for i in range(gap):
+            # range的step需要定义为步长，即可取到一个区间的所有数据
+            # 插入排序需要从第二个元素开始循环，所以开始位置不能是i,而是i+gap
+            for j in range(i+gap, length, gap):
+                temp = alist[j]
+                position = j
+
+                while position > 0 and alist[position] > temp:
+                    alist[position] = alist[position-gap]
+                    position = position-gap
+
+                alist[position] = temp
+
+    return alist
+
 
 if __name__ == '__main__':
     test_list = [random.randrange(0, 1000) for i in range(10)]
@@ -99,3 +133,4 @@ if __name__ == '__main__':
     print bubbleSort2(list(test_list))
     print selectionSort(list(test_list))
     print insertSort(list(test_list))
+    print shellSort(list(test_list))
