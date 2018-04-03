7b7e194bb01811833051fbdd88733e43ff58ac9d
angcbi <angcbi@126.com>
2018-04-03 08:21:31 +0100

    words

---
+++ graph/words.py
@@ -0,0 +1,40 @@
+# -*- coding:utf-8 -*-
+from pythonds.graphs import Graph, Vertex
+from pythonds.basic import Queue
+
+def buildGraph(wordFile):
+    d = {}
+    g = Graph()
+    wfile = open(wordFile, 'r')
+    for line in wfile:
+        word = line[:-1]
+        for i in range(len(word)):
+            bucket = word[:i] + '_' + word[i+1:]
+            if bucket in d:
+                d[bucket].append(word)
+            else:
+                d[bucket] = [word]
+
+
+    for bucket in d:
+        for word1 in d[bucket]:
+            for word2 in d[bucket]:
+                if word1 != word2:
+                    g.addEdge(word1, word2)
+
+
+def bfs(g, start):
+    start.setDistance(0)
+    start.setPred(None)
+    vertQueue = Queue()
+    vertQueue.enqueue(start)
+    while (vertQueue.size() > 0):
+        currentVert = vertQueue.dequeue()
+        for nbr in currentVert.getConnections():
+            if (nbr.getColor() == 'white'):
+                nbr.setColor('gray')
+                nbr.setDistance(currentVert.getDistance() + 1)
+                nbr.setPred(currentVert)
+                vertQueue.eenqueue(nbr)
+
+        currentVert.setColor('black')
